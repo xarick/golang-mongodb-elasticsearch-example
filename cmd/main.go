7148963b129b3b8f64e0a002b36d4cd2d6423ad8
Bobirjon Tohirov <tokhirovbob@gmail.com>
2025-01-24 17:34:22 +0500

    Create main.go

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/xarick/golang-mongodb-elasticsearch-example/config"
+	"github.com/xarick/golang-mongodb-elasticsearch-example/internal/db"
+	"github.com/xarick/golang-mongodb-elasticsearch-example/internal/handlers"
+	"github.com/xarick/golang-mongodb-elasticsearch-example/internal/routes"
+	"github.com/xarick/golang-mongodb-elasticsearch-example/internal/services"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	mongo := db.NewMongoDB(cfg.MongoURL)
+	elastic := db.NewElasticDB(cfg.ElasticURL, cfg.ElasticIndex)
+
+	newsService := services.NewNewsService(mongo, elastic)
+	newsHandler := handlers.NewNewsHandler(newsService)
+	healthHandler := handlers.NewHealthHandler()
+
+	r := routes.SetupRouter(newsHandler, healthHandler)
+
+	if err := r.Run(cfg.RunPort); err != nil {
+		log.Fatalf("Serverda xatolik: %v", err)
+	}
+}
